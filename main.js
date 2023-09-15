3b0a00acc82459f49e5125405c23c228279403a9
Andrii Sakhno <andysakhno@gmail.com>
2023-09-15 20:35:01 +0300

    Removed 'onclick' event and replaced it with 'addEventListener' + added 'onKeyDown' feature

--- main.js
+++ main.js
@@ -20,6 +20,32 @@ function autoPlay() {
         isAutoPlaying = false;
     }
 }
+
+document.querySelector('.js-rock-button')
+    .addEventListener('click', () => {
+        playGame('rock');
+    });
+
+document.querySelector('.js-paper-button')
+    .addEventListener('click', () => {
+        playGame('paper');
+    });
+
+document.querySelector('.js-scissors-button')
+    .addEventListener('click', () => {
+        playGame('scissors');
+    });
+
+document.body.addEventListener('keydown', (event) => {
+    if (event.key === 'r') {
+        playGame('rock');
+    } else if (event.key === 'p') {
+        playGame('paper');
+    } else if (event.key === 's') {
+        playGame('scissors');
+    }
+})
+
 function playGame(playerMove) {
     const computerMove = pickComputerMove();
     let result = '';
